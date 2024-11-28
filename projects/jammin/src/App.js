a69ba3b3968b251e8649f97310b838efc03bd4af
kiddulu916 <dat1kidd916@gmail.com>
2024-11-28 11:58:39 -0800

    started whole project over

--- projects/jammin/src/App.js
+++ projects/jammin/src/App.js
@@ -1,28 +1,95 @@
-import { loginEndpoint } from './Spotify';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const CLIENT_ID = "637204625e214cd6b4af3886a8a8dc24";
+const CLIENT_SECRET = "788297699d804a2caf859ead9ed10aa9";
+const REDIRECT_URI = "http://localhost:3000/";
+const SCOPE = "user-library-read user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private";
+
+const generateRandomString = (length) => {
+    return crypto.randomBytes(60).toString('hex').slice(0, length);
+}
+const STATE = generateRandomString(16);
 
 function App() {
     const [searchInput, setSearchInput] = useState("");
+    const [authCode, setAuthCode] = useState("");
+    const [accessToken, setAccessToken] = useState("");
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        var loginParams = {
+            method: 'GET',
+            body: 'response_type=code&client_id=' + CLIENT_ID + '&scope=' + SCOPE + '&redirect_uri=' + REDIRECT_URI + '&state=' + STATE + '&show_dialog=true'
+        }
+        fetch('https://accounts.spotify.com/authorize?', loginParams)
+            .then(response => response.json())
+            .then(data => { console.log(data) })
+    }, [])
+
+    useEffect(() => {
+        var authParams = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: '?grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+        }
+        fetch('https://accounts.spotify.com/api/token', authParams)
+            .then(result => result.json())
+            .then(data => setAccessToken(data.access_token)) 
+    }, [])
+
+    async function search() {
+        var searchParams = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }
+        var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParams)
+            .then(response => response.json())
+            .then(data => { return data.artists.items[0].id })
+
+        var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums?include_groups=album&market=US&limit=50', searchParams)
+            .then(response => response.json)
+            .then(data => { setAlbums(data.items) });
+    }
 
     return (
         <div className="App">
             <Container>
-                <InputGroup className="mb-3" size="lg">
+                <InputGroup className="mb-3 mt-2" size="lg">
                     <FormControl 
-                        placeholder="Search For Artist"
+                        placeholder="Search By Artist, Album, or Song"
                         type="input"
                         onKeyDown={event => {
-                            if (event.key == "Enter") {
-                                console.log("Pressed enter");
+                            if (event.key === "Enter") {
+                                search();
                             }
                         }}
                         onChange={event => setSearchInput(event.target.value)}
                     />
-                    <Button onClick={() => {console.log("Clicked Button")}}>Search</Button>
+                    <Button onClick={search}>Search</Button>
                 </InputGroup>
+            </Container>
+            <Container>
+                <Row className="mx-2 row rows-cols-4">
+                    {albums.map( (album, i) => {
+                        
+                        return (
+                            <Card>
+                                <Card.Img src={album.images[0].url} />
+                                <Card.Body>
+                                    <Card.Title>{album.name}</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        )
+                    })}
+                </Row>
             </Container>
         </div>
     );
